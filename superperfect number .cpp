e155c1d732fb69a43554d2aecc37103268dd6b10
Zainab <30340720+zainab9n@users.noreply.github.com>
2017-12-01 10:42:58 +0300

    Add files via upload

---
+++ superperfect number .cpp
@@ -0,0 +1,34 @@
+// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//let us say that 2 is superperfect number so 
+// 2 +1 = 3 -- 3 +1 = 4 --  4/2 =2 
+//
+
+
+#include "stdS"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	int i, x, j, k;
+	cin >> x;
+	for (k = 1; k <= x; k++)
+	{
+		int y = 0, z = 0;
+		for (i = 1; i <= k; i++)
+		if (k%i == 0)
+		{
+			y = y + i;
+		}
+		for (j = 1; j <= y; j++)
+		if (y%j == 0)
+		{
+			z = z + j;
+		}
+		if (z == (k * 2))
+			cout << k << " is a super perfect no\n";
+	}
+	return 0;
+
+	return 0;
+}
+
